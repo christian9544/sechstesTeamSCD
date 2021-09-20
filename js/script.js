56839860a28ebb2b0cf782d29e1787631898dcb1
Christian Genderka <christiangenderka@tutanota.de>
2021-09-20 22:02:31 +0200

    Erweiterung des Menüs.

--- js/script.js
+++ js/script.js
@@ -3,12 +3,24 @@ function _(selctor){
     return document.querySelector(selctor);
 }
 
+function _All(selctor){
+  return document.querySelectorAll(selctor);
+}
+
 function openNav() {
   _("#myNav").style.height = "100%";
+  _("#menuButton").style.opacity = 0;
+  _All(".caption").forEach(element => {
+    element.style.opacity = 0;
+  });
 }
 
 function closeNav() {
   _("#myNav").style.height = "0%";
+  _("#menuButton").style.opacity = 1;
+  _All(".caption").forEach(element => {
+    element.style.opacity = 1;
+  });
 }
 
 _("#menuButton").addEventListener("click", function(){
@@ -17,4 +29,14 @@ _("#menuButton").addEventListener("click", function(){
 
 _(".closebtn").addEventListener("click", (event) =>{
     closeNav();
+});
+
+_All('.overlay-content a').forEach(link => {
+    link.addEventListener("click", function(){
+      closeNav();
+      _All('.overlay-content a').forEach(link2 => {
+        link2.style.fontStyle = "";
+      });
+      link.style.fontStyle = "italic";
+    });
 });
